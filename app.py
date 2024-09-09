0c669db1edab05c633878a5ec21a4bba4d5ad6bb
AJ <38798090+axjasf@users.noreply.github.com>
2024-09-09 15:43:02 -0700

    feat: basic app structure"

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("Personal Finance Categorizer")
+    st.write("Welcome to the Personal Finance Categorizer Web App!")
+
+if __name__ == "__main__":
+    main()
