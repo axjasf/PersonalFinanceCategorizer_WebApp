6323663bcfbcd8e14e2c154c40b0fe60eb8f07ff
AJ <38798090+axjasf@users.noreply.github.com>
2024-09-18 20:56:09 -0700

    refactor

--- data/tables/accounts.sql
+++ data/tables/accounts.sql
@@ -7,12 +7,3 @@ CREATE TABLE IF NOT EXISTS "accounts" (
     "bank_identifier" TEXT UNIQUE,
 	PRIMARY KEY("id")
 );
-
-
--- Categories Table
-CREATE TABLE categories (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
-    parent_id INTEGER,
-    FOREIGN KEY(parent_id) REFERENCES categories(id)
-);
